cf18a36e06feafa1bf4dfb50149731ec234f40f8
Shital Bhutiya <ssbhutiya@gmail.com>
2018-05-17 12:14:01 -0400

    v.2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-const cat = document.getElementById('catImage');
-let counter = 0;
-cat.addEventListener('click', function () {
-    document.getElementById("counter").textContent = "Counter:" + (++counter);
+const cat1 = document.getElementById('catImage1');
+let counter1 = 0,
+    counter2 = 0;
+    const cat1name="Venia",cat2name="Bella";
+    document.getElementById('cat1name').textContent=cat1name;
+    document.getElementById('cat2name').textContent=cat2name;
+cat1.addEventListener('click', function () {
+    document.getElementById("counter1").textContent = "Counter:" + (++counter1);
+});
+
+const cat2 = document.getElementById('catImage2');
+cat2.addEventListener('click', function () {
+    document.getElementById("counter2").textContent = "Counter:" + (++counter2);
 });
